ab97d503aae4076c7795a29a2b931b6f227d4964
implaymo <136201112+implaymo@users.noreply.github.com>
2024-01-30 10:48:03 +0000

    Manage to make all paths work(home, generic, elements) in main.py)

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/generic")
+def generic():
+    return render_template('generic.html')
 
-
-
+@app.route("/elements")
+def elements():
+    return render_template('elements.html')
 
 
 
