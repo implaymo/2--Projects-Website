50744061d7ca44ec70117f3cb215802fd58b5ff4
implaymo <136201112+implaymo@users.noreply.github.com>
2024-01-30 11:17:02 +0000

    Added header to login.html and signup.html. Make them with better design, but needs improvements.

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
-from wtforms import Form, StringField, validators, PasswordField
+from wtforms import Form, StringField, validators, PasswordField, SubmitField
 
 class SignUp(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     password = PasswordField("Password", [validators.Length(min=8)])
-    
-    
+    submit = SubmitField('Sign Up')  
     
 class Login(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     password = PasswordField("Password", [validators.Length(min=8)])
+    submit = SubmitField('Login') 
