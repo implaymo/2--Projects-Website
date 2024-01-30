29c7dfbb8b5ebd212f0a0184e58c7ac004d8c018
implaymo <136201112+implaymo@users.noreply.github.com>
2024-01-30 10:54:52 +0000

    Manage to make login and signup paths work. When login or signup clicked, the redirect to the singup.html and login.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from forms import Login, SignUp
 app = Flask(__name__)
 
 
@@ -15,6 +15,16 @@ def generic():
 def elements():
     return render_template('elements.html')
 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = Login()
+    return render_template('login.html', form=form)
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    form = SignUp()
+    return render_template('signup.html', form=form)
 
 
 
