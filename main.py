8b942dbc3a29bb2334e247356c325b9656841a74
implaymo <136201112+implaymo@users.noreply.github.com>
2024-01-30 13:36:13 +0000

    Create database.py to handle Flask SQLAlchemy database

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 from forms import Login, SignUp
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+
 
 
 @app.route("/", methods=["POST", "GET"])
