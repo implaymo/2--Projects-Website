f078b3280bb220cd09b57613289b6980e1e6a376
implaymo <136201112+implaymo@users.noreply.github.com>
2024-01-31 17:20:59 +0000

    Check for login validation in login() and create secret_key

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
+from flask_wtf import FlaskForm
 from wtforms import Form, StringField, validators, PasswordField, SubmitField
 
-class SignUp(Form):
+class SignUp(FlaskForm):
     username = StringField('Username', [validators.Length(max=25)])
     email = StringField('Email Address', [validators.Length(max=35)])
     password = PasswordField("Password", [validators.Length(min=1)])
     submit = SubmitField('Sign Up')  
     
-class Login(Form):
+class Login(FlaskForm):
     username = StringField('Username', [validators.Length(max=25)])
     password = PasswordField("Password", [validators.Length(min=1)])
     submit = SubmitField('Login') 
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import Login, SignUp
 from database import db, User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
+import os
+from dotenv import load_dotenv
 
-
-
-
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.secret_key = os.getenv("SECRET_KEY")
 
+
+with app.app_context():
+    db.init_app(app) 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-with app.app_context():
-    db.init_app(app) 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -32,6 +38,19 @@ def elements():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     form = Login()
+    if form.validate_on_submit():
+        user = User.get_user_by_credentials(form.username.data, form.password.data)
+        if user:
+            login_user(user)
+            flash('You were successfully logged in')
+            return redirect(url_for("home"))
+        elif not user:
+            flash("User not found. Sign up first!")
+            return redirect(url_for("signup"))
+        else:
+            flash("Invalid credentials")
+    else:
+        flash("Form validation failed", 'danger')
     return render_template('login.html', form=form)
 
 @app.route("/signup", methods=["POST", "GET"])
