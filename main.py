2f9e3242562902de3c759d92dc603b64786a1364
implaymo <goncalomorerasilva@gmail.com>
2024-02-03 11:54:42 +0000

    Change generic.html, element.html and index.html in order to dont see the login button when running as a user or admin

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.secret_key = os.getenv("SECRET_KEY")
+
 
 bcrypt = Bcrypt(app) 
 
